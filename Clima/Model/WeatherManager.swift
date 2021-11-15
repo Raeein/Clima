c74bb9e7867ed81f1ed5c94385f665a9e036d29b
Raeein Bagheri <raeeinbagheri80@gmail.com>
2021-11-15 10:34:36 -0500

    Implement JSONDecoder

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -14,20 +14,31 @@ struct WeatherMamager {
             //Create URLSessiom
             let session = URLSession(configuration: .default)
             //Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    //Add self because in a closure
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             //Start the task
             task.resume()
         }
         
     }
-    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
-        if error != nil {
-            print(error!)
-            return
-        }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        //.self to show the type of an object
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
+        
 }
