988fc81b8c4e64656a82e8334b84aec449c250b1
Raeein Bagheri <raeeinbagheri80@gmail.com>
2021-11-14 21:46:19 -0500

    Implement URLSession

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -1,9 +1,33 @@
+import Foundation
+
 struct WeatherMamager {
     let API_KEY = Constants().OpenWeatherAppApiKey
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)&appid=\(API_KEY)&units=metric"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    func performRequest(urlString: String) {
+        //Create URL
+        if let url = URL(string: urlString) {
+            //Create URLSessiom
+            let session = URLSession(configuration: .default)
+            //Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            //Start the task
+            task.resume()
+        }
+        
+    }
+    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
